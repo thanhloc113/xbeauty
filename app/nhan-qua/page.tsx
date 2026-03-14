2b13d92e2b9d2cfdd0979a7c3ffc7854e19f16dd
loc <leethanhloc1995@gmail.com>
2026-03-14 17:06:56 +0700

    update new page

--- app/nhan-qua/page.tsx
+++ app/nhan-qua/page.tsx
@@ -10,7 +10,6 @@ export default function Page() {
     <main>
 
       <Navbar />
-
       <Hero
         as="h1"
         title="Em bé cần anh giúp gì nè ?"
@@ -21,24 +20,20 @@ export default function Page() {
       <Gallery >
         <Item
           title="💖 Em muốn xinh đẹp 💖"
-          media="https://i.pinimg.com/736x/b3/88/b4/b388b419365b411aa8ba4c35a1cf2ecd.jpg"
+          media="video/video4.mp4"
           ratio="vertical"
+          type="video"
           link="/xinh-dep"
-          buttonText="Anh ơi"
+          buttonText="Anh ơii"
         />
         <Item
           title="💓Em muốn đi biển 💓"
-          media="image/sexy2.jpg"
+          media="video/video5.mp4"
           ratio="vertical"
+          type="video"
           link="/di-bien"
-          buttonText="Anh ưi"
-        />
-        <Item
-          title="💖 Em muốn nhận quà 💖"
-          media="https://i.pinimg.com/736x/30/aa/cb/30aacb23fd69a74a66038304fef431a2.jpg"
-          ratio="vertical"
-          link="/nhan-qua"
-          buttonText="Anh iu"
+          buttonText="Anh ưii"
+          zoomVideo={1.1}
         />
     </Gallery>
 
