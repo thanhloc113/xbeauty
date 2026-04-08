8b2cf4b44537e89a5b47a3bfc29654840f1221ae
loc <leethanhloc1995@gmail.com>
2026-04-08 13:36:13 +0700

    update layout

--- components/ProductReviewSlider.tsx
+++ components/ProductReviewSlider.tsx
@@ -234,7 +234,7 @@ const handleTogglePlay = () => {
     <div className="flex flex-col w-full h-full rounded-xl overflow-hidden">
 
       {/* HEADER */}
-<div className="flex flex-col px-3 py-2 text-pink">
+<div className="flex flex-col px-3 text-pink">
 
   {/* TOP: LENGTH */}
   <div className="text-xs leading-none">
@@ -242,7 +242,7 @@ const handleTogglePlay = () => {
   </div>
 
   {/* BOTTOM: PRODUCT NAME */}
-  <div className="mt-1 text-center">
+  <div className="mt-1 text-center py-2">
     <div className="font-semibold leading-tight">
       {productNameLines.map((line, idx) => (
         <div
@@ -287,8 +287,8 @@ const handleTogglePlay = () => {
 
         {/* NAV */}
         <div className="absolute top-1/2 left-0 right-0 flex justify-between px-4 z-20">
-          <button onClick={prev} className="bg-white/10 px-3 py-2 rounded-full">◀</button>
-          <button onClick={next} className="bg-white/10 px-3 py-2 rounded-full">▶</button>
+          <button onClick={prev} className="bg-white/5 px-2 py-2 rounded-full">◀</button>
+          <button onClick={next} className="bg-white/5 px-2 py-2 rounded-full">▶</button>
         </div>
 
         {/* CAPTION + MUA NGAY */}
