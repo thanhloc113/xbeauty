ccb93aaa308012f8d2aa76d9e4f9411fb3829350
loc <leethanhloc1995@gmail.com>
2026-09-11 23:05:23 +0700

    update new data

--- data/products.ts
+++ data/products.ts
@@ -23,7 +23,7 @@ export const Videos: Video[] = [
     },
     {
      url:"https://ik.imagekit.io/7a7njhuhn/snaptik.vn_GXsPq.mp4",
-     title:"Xinh đẹp mình đi đến chỗ đẹp",
+     title:"Xinh đẹp mình đi ngắm cảnh đẹp",
      poster:"https://i.pinimg.com/736x/63/34/39/63343907bd30b92c4205cf8be7f52427.jpg"
     },
     {
